e76cac13167184dd7515e26d772c5c7c73b9bd1a
hubarthurcoelho <arthurcoelho7885@gmail.com>
2022-11-05 18:05:46 -0300

    organizando as pastas de css. Agora existe um arquivo para cada sessao estilizada

--- front-end-next/pages/_app.js
+++ front-end-next/pages/_app.js
@@ -1,4 +1,9 @@
 import '../styles/globals.css';
+import '../styles/catalog.css';
+import '../styles/header.css';
+import '../styles/burgerMenu.css';
+import '../styles/footer.css';
+import '../styles/login.css';
 import React from 'react';
 import { HeaderProvider, AuthProvider } from '../contexts';
 
